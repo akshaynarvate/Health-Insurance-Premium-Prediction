1cd6ffb97bebace5e65cc271a1003fc5207e6846
Akshay Narvate <88389553+akshaynarvate@users.noreply.github.com>
2022-05-23 22:56:14 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+import pickle
+import warnings
+
+warnings.filterwarnings("ignore")
+model = pickle.load(open("Model_Health_Insurance.pkl","rb")) #loading the created model
+
+
+st.set_page_config(page_title="Health Inusrance Premium Prediciton Application") #tab title
+
+#prediction function
+def predict_status(age, sex, bmi, smoker):
+    input_data = np.asarray([age, sex, bmi, smoker])
+    input_data = input_data.reshape(1,-1)
+    prediction = model.predict(input_data)
+    return prediction[0]
+
+def main():
+
+    # titling your page
+    st.title("Health Insurance Premium Prediction App")
+
+    #getting the input
+    age = st.number_input("Enter your Age")
+    sex = st.number_input("Enter your sex")
+    bmi = st.number_input("Enter your BMI Index")
+    smoker = st.number_input("Enter your smoker status(0-No/1-Yes")
+
+    #predict value
+    diagnosis = ""
+
+    if st.button("Predict"):
+    
+        diagnosis = predict_status(age, height, weight)
+        if diagnoisis:
+            st.write(diagnosis, "Rs")
+
+        else:
+            st.error("Please Re-Enter your details")
+        
+        st.write("Project by Akshay Narvate")
+    
+            
+if __name__=="__main__":
+    main()
